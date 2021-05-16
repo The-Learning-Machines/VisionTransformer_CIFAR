2a6d63de671147ed36286d60a0b06620ee2f36a7
Sai Mitheran <49593646+smj007@users.noreply.github.com>
2021-05-16 21:00:24 +0530

    fix dataloader

--- dataloader.py
+++ dataloader.py
@@ -17,15 +17,26 @@ class dataloader(torch.utils.data.Dataset):
         data = self.data[idx]
         label = data[1]
         image = np.array(data[0])
+
         if self.transforms:
             image = self.transforms(image=image)['image']
 
-        image = torch.tensor(image, dtype=torch.float)
-        image = image.unfold(0, config.patch_size, config.patch_size).unfold(1, config.patch_size, config.patch_size)
-        image = image.reshape(image.shape[0], image.shape[1], image.shape[2]*image.shape[3]*image.shape[4])
+        image = torch.tensor(image, dtype=torch.float).unsqueeze(0).permute(0, 3, 1, 2)
+        image = image.unfold(2, config.patch_size, config.patch_size).unfold(3, config.patch_size, config.patch_size)
+        image = image.permute(0, 2, 3, 1, 4, 5)
+        image = image.reshape(
+            image.shape[0],
+            image.shape[1],
+            image.shape[2],
+            image.shape[3]*image.shape[4]*image.shape[5]
+        )
+        image = image.view(image.shape[0], -1, image.shape[-1]) 
         image = image.view(-1, image.shape[-1])
-
+        
+        
         return {
             'patches' : image,
             'label' : torch.tensor(label, dtype=torch.long),
         }
+
+      
